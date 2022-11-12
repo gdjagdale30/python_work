4e173d31aeac564a7b6c374a96188cd6d5e91482
gdjagdale <ganeshjagdale30@gmail.com>
2022-11-12 13:09:42 +0530

    Flow control

---
+++ flow_control.py
@@ -0,0 +1,38 @@
+#To print sum of numbers present inside list
+
+# list=eval(input("Enter list:  "))
+# sum=0
+# for x in list:
+#     sum=sum+x
+#     print("The Sum = ",sum)
+
+
+
+#To display the sum of n numbers
+
+# n=int(input("Enter number: "))
+# sum=0
+# i=1
+# while i<=n:
+#     sum=sum+i
+#     i=i+1
+#     print("The sum of first:",n,"numbers is: ",sum)
+
+
+
+#Infinite Loops
+#Write a program to display '*'s in right angled triangled from
+# n=int(input("Enter number of rows: "))
+
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print("* ",end=" ")
+#     print()
+
+#Write a program to display * s in display 
+# n=int(input("Enter number of rows: "))
+# for i in range(1,n+1):
+#     print(" "*(n-1),end=" ")
+#     print("* "*i)
+
+
